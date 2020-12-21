a89490d52891f5d08ad47029b1c4dae71a375533
Neeraj Patil <patilneeraj2212@gmail.com>
2020-12-22 01:20:33 +0530

    initial setup

---
+++ MERN-skeleton/server/server.js
@@ -0,0 +1,24 @@
+import app from "./express";
+import config from "./../config/config";
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+
+mongoose
+	.connect(config.mongoUri, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then((res) => console.log("Database connected successfully"));
+
+mongoose.connection.on("error", () => {
+	throw new Error(`unable to connect to database `);
+});
+
+app.listen(config.port, (err) => {
+	if (err) {
+		console.log(err);
+	}
+	console.info("Server started on port %s.", config.port);
+});
