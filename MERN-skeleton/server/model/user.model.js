aabde1cb829b12a0271059ff5db5f86f32849caf
Neeraj Patil <patilneeraj2212@gmail.com>
2020-12-24 12:57:20 +0530

    schema and error handling

---
+++ MERN-skeleton/server/model/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: "Name is required",
+		},
+		email: {
+			type: String,
+			trim: true,
+			unique: "Email already exists",
+			match: [/.+\@.+\..+/, "Please fill a valid email address"],
+			required: "Email is required",
+		},
+		password: {
+			type: String,
+			required: "Password is required",
+		},
+	},
+	{ timestamps: true }
+);
+
+UserSchema.path("password").validate(function (v) {
+	if (this.password && this.password.length < 6) {
+		this.invalidate("password", "Password must be at least 6 characters.");
+	}
+	if (this.isNew && !this.password) {
+		this.invalidate("password", "Password is required");
+	}
+}, null);
+
+UserSchema.methods = {
+	authenticate: async function (plainText) {
+		try {
+			return await bcrypt.compare(plainText, this.hashed_password);
+		} catch (error) {
+			console.log(error.message);
+			return false;
+		}
+	},
+	encryptPassword: async function (password) {
+		if (!password) return "";
+
+		try {
+			let hash = await bcrypt.hash(password, 10);
+			return hash;
+		} catch (error) {
+			console.log(error);
+			return "";
+		}
+	},
+};
+
+UserSchema.pre("save", async function (next) {
+	this.password = await this.encryptPassword(this.password);
+	next();
+});
+
+export default mongoose.model("User", UserSchema);
